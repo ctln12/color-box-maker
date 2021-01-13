6c834d0ddc4143c519ab1379e0a40c1ef27d5d2b
Cataline Baert <snuf_kin@yahoo.com>
2021-01-13 22:03:21 +0100

    create deleteBox function in BoxList component and pass it to Box component

--- src/BoxList.js
+++ src/BoxList.js
@@ -8,8 +8,14 @@ import { v4 as uuidv4 } from 'uuid';
 class BoxList extends Component {
   constructor(props){
     super(props);
-    this.state = { boxes: [] };
+    this.state = { boxes: [
+      { width: "180", height: "190", backgroundColor: "purple", id: uuidv4() },
+      { width: "190", height: "200", backgroundColor: "pink", id: uuidv4() },
+      { width: "210", height: "220", backgroundColor: "violet", id: uuidv4() },
+      { width: "215", height: "205", backgroundColor: "darkblue", id: uuidv4() }
+    ] };
     this.addBox = this.addBox.bind(this);
+    this.deleteBox = this.deleteBox.bind(this);
   }
   addBox(box){
     let newBox = { ...box, id: uuidv4() };
@@ -17,12 +23,17 @@ class BoxList extends Component {
       boxes: [...state.boxes, newBox]
     }))
   }
+  deleteBox(boxToDelete){
+    this.setState(state => ({
+      boxes: state.boxes.filter(box => box.id !== boxToDelete.id)
+    }));
+  }
   render() {
     return (
       <div className="BoxList">
         <NewBoxForm addBox={this.addBox} />
         <div>
-          {this.state.boxes.map(box => <Box key={box.id} width={box.width} height={box.height} backgroundColor={box.backgroundColor} />)}
+          {this.state.boxes.map(box => <Box key={box.id} id={box.id} width={box.width} height={box.height} backgroundColor={box.backgroundColor} deleteBox={this.deleteBox} />)}
         </div>
       </div>
     );
